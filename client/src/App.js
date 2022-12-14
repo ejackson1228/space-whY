598b7094b3f1eae2fac8291f6bbe305222f0edca
Charlie Carter <charliec1665@gmail.com>
2022-12-14 18:06:15 -0500

    client basic setup

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql'
+});
+
+// indexedDB authorization right? May need to change this
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem('id_token');
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     }
+//   };
+// });
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
