91d02b08f6505640d8b7f397e42e329921e66623
Charlie Carter <charliec1665@gmail.com>
2022-12-18 15:39:02 -0500

    checking out zai's branch for page info and integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React, { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
 import React from 'react';
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { setContext } from '@apollo/client/link/context';
 
+import Header from './components/Header';
 import Footer from './components/Footer';
 
 const httpLink = createHttpLink({
