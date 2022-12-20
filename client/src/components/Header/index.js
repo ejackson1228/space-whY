b32c9f94065f7600b4434880cef87c0874877f8d
Myeisha Swain <zse0296@gmail.com>
2022-12-20 15:53:39 -0500

    fixed nav bar link issues

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,7 +8,7 @@ function Header({ currentPage, handlePageChange }) {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container className='d-flex'>
-        <Navbar.Brand className="navbar-brand d-flex align-items-center text-light" href="home">Inkling</Navbar.Brand>
+        <Navbar.Brand className="navbar-brand d-flex align-items-center text-light" href="/">Inkling</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end text-light'>
 
@@ -22,7 +22,7 @@ function Header({ currentPage, handlePageChange }) {
               <Nav.Link href="Profile" onClick={() => handlePageChange('Profile')}>Profile</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="Feed" onClick={() => handlePageChange('Feed')}>Feed</Nav.Link>
+              <Nav.Link href="/" onClick={() => handlePageChange('Feed')}>Feed</Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar.Collapse>
