f507f85f3fc43713c2d832f413c8229351469695
Myeisha Swain <zse0296@gmail.com>
2022-12-19 20:56:20 -0500

    trying to commit

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -5,6 +5,8 @@ import { LOGIN_USER } from '../utils/mutations';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import Auth from '../utils/auth';
 
